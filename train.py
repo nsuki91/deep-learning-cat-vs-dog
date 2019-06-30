dfc9a79f70985cb28d9d7296407666a6b5aa829c
Mert <40965225+nsuki91@users.noreply.github.com>
2019-06-30 21:00:59 +0300

    Add files via upload

---
+++ train.py
@@ -0,0 +1,137 @@
+# python train.py -o normal (if you generating a new model)
+# python train.py -o train (if you have existing trained model)
+#
+# libraries
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import cv2
+from random import shuffle
+from tqdm import tqdm
+import argparse
+import os, sys
+
+
+# arguments for operation selection
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--operation", required=True)
+args = vars(ap.parse_args())
+
+
+# setting up variables
+
+TRAIN_DIR = 'D:/Workspace/train'
+TEST_DIR = 'D:/Workspace/test'
+IMG_SIZE = 50
+LR = 1e-3
+CATEGORIES = ["Dog", "Cat"]
+epoch = 5
+MODEL_NAME = 'dogsvscats-{}-{}.model'.format(LR, '6conv-basic')
+
+
+# labeling function
+
+def label_img(img): 
+    word_label = img.split('.')[-3]
+    if word_label == 'cat':
+        return  [1,0]
+    elif word_label == 'dog':
+        return [0,1]
+
+
+# generating train data function
+
+def create_train_data():
+    training_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR,img)
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        training_data.append([np.array(img),np.array(label)])
+    shuffle(training_data)
+    np.save('train_data.npy', training_data)
+    return training_data
+
+
+# processing of test data function
+
+def process_test_data():
+    testing_data = []
+    for img in tqdm(os.listdir(TEST_DIR)):
+        path = os.path.join(TEST_DIR,img)
+        img_num = img.split('.')[0]
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        testing_data.append([np.array(img), img_num])
+        
+    shuffle(testing_data)
+    np.save('test_data.npy', testing_data)
+    return testing_data
+
+
+# training existing or new generated model
+
+def start_training():
+    train_data = np.load('train_data.npy')
+    tf.reset_default_graph()
+
+    convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+    convnet = conv_2d(convnet, 32, 2, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 64, 2, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 32, 2, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 64, 2, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 32, 2, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 64, 2, activation='relu')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = fully_connected(convnet, 1024, activation='relu')
+    convnet = dropout(convnet, 0.8)
+
+    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+
+    if os.path.exists('{}.meta'.format(MODEL_NAME)):
+        model.load(MODEL_NAME)
+        print('Model loaded.')
+
+    train = train_data[:-500]
+    test = train_data[-500:]
+
+    X = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    Y = [i[1] for i in train]
+
+    test_x = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    test_y = [i[1] for i in test]
+
+    model.fit({'input': X}, {'targets': Y}, n_epoch=epoch, validation_set=({'input': test_x}, {'targets': test_y}), 
+        snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+    model.save(MODEL_NAME)
+    print("Training process completed!")
+
+if args["operation"] == "train":
+    start_training()
+elif args["operation"] == "normal":
+    train_data = create_train_data()
+    start_training()
+
